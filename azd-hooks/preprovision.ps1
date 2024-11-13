edc72af82526d975d2d5242cf585590763b2a7b4
Ryan Graham <rygraham@microsoft.com>
2024-11-13 09:36:46 -0500

    Optimizing the preprovision hooks

--- azd-hooks/preprovision.ps1
+++ azd-hooks/preprovision.ps1
@@ -2,33 +2,53 @@
 
 Write-Host "Ensuring Azure CLI extensions and dependencies are installed"
 
+# add azure cli extensions
+az extension add --upgrade --name "aks-preview"
+
 az provider register --namespace "Microsoft.ContainerService"
 while ((az provider show --namespace "Microsoft.ContainerService" --query "registrationState" -o tsv) -ne "Registered") {
   Write-Host "Waiting for Microsoft.ContainerService provider registration..."
   Start-Sleep -Seconds 3
 }
 
-az feature register --namespace "Microsoft.ContainerService" --name "NetworkObservabilityPreview"
-while ((az feature show --namespace "Microsoft.ContainerService" --name "NetworkObservabilityPreview" --query "properties.state" -o tsv) -ne "Registered") {
-  Write-Host "Waiting for NetworkObservabilityPreview feature registration..."
+# Register the preview features
+az feature register --namespace "Microsoft.ContainerService" --name "EnableAPIServerVnetIntegrationPreview"
+az feature register --namespace "Microsoft.ContainerService" --name "NRGLockdownPreview"
+az feature register --namespace "Microsoft.ContainerService" --name "SafeguardsPreview"
+az feature register --namespace "Microsoft.ContainerService" --name "NodeAutoProvisioningPreview"
+az feature register --namespace "Microsoft.ContainerService" --name "DisableSSHPreview"
+az feature register --namespace "Microsoft.ContainerService" --name "AutomaticSKUPreview"
+
+
+while ((az feature show --namespace "Microsoft.ContainerService" --name "EnableAPIServerVnetIntegrationPreview" --query "properties.state" -o tsv) -ne "Registered") {
+  Write-Host "Waiting for EnableAPIServerVnetIntegrationPreview feature registration..."
   Start-Sleep -Seconds 3
 }
 
-az feature register --namespace "Microsoft.ContainerService" --name "NodeOsUpgradeChannelPreview"
-while ((az feature show --namespace "Microsoft.ContainerService" --name "NodeOsUpgradeChannelPreview" --query "properties.state" -o tsv) -ne "Registered") {
-  Write-Host "Waiting for NodeOsUpgradeChannelPreview feature registration..."
+while ((az feature show --namespace "Microsoft.ContainerService" --name "NRGLockdownPreview" --query "properties.state" -o tsv) -ne "Registered") {
+  Write-Host "Waiting for NRGLockdownPreview feature registration..."
   Start-Sleep -Seconds 3
 }
 
-az feature register --namespace "Microsoft.ContainerService" --name "AzureMonitorMetricsControlPlanePreview" 
-while ((az feature show --namespace "Microsoft.ContainerService" --name "AzureMonitorMetricsControlPlanePreview" --query "properties.state" -o tsv) -ne "Registered") {
-  Write-Host "Waiting for AzureMonitorMetricsControlPlanePreview feature registration..."
+while ((az feature show --namespace "Microsoft.ContainerService" --name "SafeguardsPreview" --query "properties.state" -o tsv) -ne "Registered") {
+  Write-Host "Waiting for SafeguardsPreview feature registration..."
+  Start-Sleep -Seconds 3
+}
+
+while ((az feature show --namespace "Microsoft.ContainerService" --name "NodeAutoProvisioningPreview" --query "properties.state" -o tsv) -ne "Registered") {
+  Write-Host "Waiting for NodeAutoProvisioningPreview feature registration..."
+  Start-Sleep -Seconds 3
+}
+
+while ((az feature show --namespace "Microsoft.ContainerService" --name "DisableSSHPreview" --query "properties.state" -o tsv) -ne "Registered") {
+  Write-Host "Waiting for DisableSSHPreview feature registration..."
+  Start-Sleep -Seconds 3
+}
+
+while ((az feature show --namespace "Microsoft.ContainerService" --name "AutomaticSKUPreview" --query "properties.state" -o tsv) -ne "Registered") {
+  Write-Host "Waiting for AutomaticSKUPreview feature registration..."
   Start-Sleep -Seconds 3
 }
 
 # propagate the feature registrations
-az provider register -n Microsoft.ContainerService
-
-# add azure cli extensions
-az extension add --upgrade --name aks-preview
-az extension add --upgrade --name amg
+az provider register --namespace "Microsoft.ContainerService"
