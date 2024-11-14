1c603da40e20f900f2598dd0e90bdf1d7ed13558
Ryan Graham <rygraham@microsoft.com>
2024-11-13 23:18:44 -0500

    Stuff

--- azd-hooks/postprovision.ps1
+++ azd-hooks/postprovision.ps1
@@ -3,12 +3,12 @@
 $services=@("KubeCon.Sk.Debate.Host", "KubeCon.Sk.Debate.Leaderboard", "KubeCon.Sk.Debate.DefaultAgents")
 
 if (($env:DEPLOY_AZURE_CONTAINER_REGISTRY -like "true") -and ($env:BUILD_CONTAINERS -like "true")) {
-  echo "Build container images"
+  Write-Output "Build container images"
   foreach ($service in $services) {
-    echo "Building kubecon-sk-debate/${service}:latest"
+    Write-Output "Building kubecon-sk-debate/${service}:latest"
     az acr build --registry $env:AZURE_REGISTRY_NAME --image kubecon-sk-debate/${service}:latest ./src/${service}/
   }
 } 
 else {
-  echo "No BUILD_CONTAINERS variable set, skipping container build"
+  Write-Output "No BUILD_CONTAINERS variable set, skipping container build"
 }
