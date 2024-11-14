9d84f7aa16e64c274ebfc7a186c375190b22103e
Ryan Graham <rygraham@microsoft.com>
2024-11-14 02:53:54 -0500

    Adding postprovision back into the mix.

--- azd-hooks/predeploy.ps1
+++ azd-hooks/predeploy.ps1
@@ -1,42 +1,41 @@
 #!/usr/bin/env pwsh
 
-##########################################################
 # Check kubelogin and install if not exists
-##########################################################
+
 if (-not (Get-Command kubelogin -ErrorAction SilentlyContinue)) {
   az aks install-cli
 }
 
-###########################################################
-# Create the custom-values.yaml file
-###########################################################
-@"
-namespace: ${env:AZURE_AKS_NAMESPACE}
-"@ | Out-File -FilePath custom-values.yaml -Encoding utf8
+# ###########################################################
+# # Create the custom-values.yaml file
+# ###########################################################
+# @"
+# namespace: ${env:AZURE_AKS_NAMESPACE}
+# "@ | Out-File -FilePath custom-values.yaml -Encoding utf8
 
-###########################################################
-# Add Azure Managed Identity and set to use AzureAD auth 
-###########################################################
-if (![string]::IsNullOrEmpty($env:AZURE_IDENTITY_CLIENT_ID) -and ![string]::IsNullOrEmpty($env:AZURE_IDENTITY_NAME)) {
-@"
-useAzureAd: true
-managedIdentityName: $($env:AZURE_IDENTITY_NAME)
-managedIdentityClientId: $($env:AZURE_IDENTITY_CLIENT_ID)
-"@ | Out-File -Append custom-values.yaml
-}
+# ###########################################################
+# # Add Azure Managed Identity and set to use AzureAD auth 
+# ###########################################################
+# if (![string]::IsNullOrEmpty($env:AZURE_IDENTITY_CLIENT_ID) -and ![string]::IsNullOrEmpty($env:AZURE_IDENTITY_NAME)) {
+# @"
+# useAzureAd: true
+# managedIdentityName: $($env:AZURE_IDENTITY_NAME)
+# managedIdentityClientId: $($env:AZURE_IDENTITY_CLIENT_ID)
+# "@ | Out-File -Append custom-values.yaml
+# }
 
-###########################################################
-# Add base images
-###########################################################
-@"
-namespace: ${env:AZURE_AKS_NAMESPACE}
-debate-host:
-  image:
-    repository: ${env:AZURE_REGISTRY_URI}/kubecon-sk-debate/debate-host
-leaderboard:
-  image:
-    repository: ${env:AZURE_REGISTRY_URI}/kubecon-sk-debate/leaderboard
-default-agents:
-  image:
-    repository: ${env:AZURE_REGISTRY_URI}/kubecon-sk-debate/default-agents
-"@ | Out-File -Append custom-values.yaml
+# ###########################################################
+# # Add base images
+# ###########################################################
+# @"
+# namespace: ${env:AZURE_AKS_NAMESPACE}
+# debate-host:
+#   image:
+#     repository: ${env:AZURE_REGISTRY_URI}/kubecon-sk-debate/debate-host
+# leaderboard:
+#   image:
+#     repository: ${env:AZURE_REGISTRY_URI}/kubecon-sk-debate/leaderboard
+# default-agents:
+#   image:
+#     repository: ${env:AZURE_REGISTRY_URI}/kubecon-sk-debate/default-agents
+# "@ | Out-File -Append custom-values.yaml
--- azd-hooks/predeploy.sh
+++ azd-hooks/predeploy.sh
@@ -1,43 +1,41 @@
 #!/bin/bash
 
-##########################################################
 # Check kubelogin and install if not exists
-##########################################################
 if ! command -v kubelogin &> /dev/null; then
   echo "kubelogin could not be found. Installing kubelogin..."
   az aks install-cli
 fi
 
-##########################################################
-# Create the custom-values.yaml file
-##########################################################
-cat << EOF > custom-values.yaml
-namespace: ${AZURE_AKS_NAMESPACE}
-EOF
+# ##########################################################
+# # Create the custom-values.yaml file
+# ##########################################################
+# cat << EOF > custom-values.yaml
+# namespace: ${AZURE_AKS_NAMESPACE}
+# EOF
 
-###########################################################
-# Add Azure Managed Identity and set to use AzureAD auth 
-###########################################################
-if [ -n "${AZURE_IDENTITY_CLIENT_ID}" ] && [ -n "${AZURE_IDENTITY_NAME}" ]; then
-  cat << EOF >> custom-values.yaml
-useAzureAd: true
-managedIdentityName: ${AZURE_IDENTITY_NAME}
-managedIdentityClientId: ${AZURE_IDENTITY_CLIENT_ID}
-EOF
-fi
+# ###########################################################
+# # Add Azure Managed Identity and set to use AzureAD auth 
+# ###########################################################
+# if [ -n "${AZURE_IDENTITY_CLIENT_ID}" ] && [ -n "${AZURE_IDENTITY_NAME}" ]; then
+#   cat << EOF >> custom-values.yaml
+# useAzureAd: true
+# managedIdentityName: ${AZURE_IDENTITY_NAME}
+# managedIdentityClientId: ${AZURE_IDENTITY_CLIENT_ID}
+# EOF
+# fi
 
-##########################################################
-# Add base images
-##########################################################
-cat << EOF >> custom-values.yaml
-namespace: ${AZURE_AKS_NAMESPACE}
-debate-host:
-  image:
-    repository: ${AZURE_REGISTRY_URI}/kubecon-sk-debate/debate-host
-leaderboard:
-  image:
-    repository: ${AZURE_REGISTRY_URI}/kubecon-sk-debate/leaderboard
-default-agents:
-  image:
-    repository: ${AZURE_REGISTRY_URI}/kubecon-sk-debate/default-agents
-EOF
+# ##########################################################
+# # Add base images
+# ##########################################################
+# cat << EOF >> custom-values.yaml
+# namespace: ${AZURE_AKS_NAMESPACE}
+# debate-host:
+#   image:
+#     repository: ${AZURE_REGISTRY_URI}/kubecon-sk-debate/debate-host
+# leaderboard:
+#   image:
+#     repository: ${AZURE_REGISTRY_URI}/kubecon-sk-debate/leaderboard
+# default-agents:
+#   image:
+#     repository: ${AZURE_REGISTRY_URI}/kubecon-sk-debate/default-agents
+# EOF
